9b28aae6e7bc7a8d1d65f5aa00071435d31433e1
Olivia G <gleason.olivia@icloud.com>
2026-05-21 20:52:47 -0700

    .

--- src/data/dataloader.py
+++ src/data/dataloader.py
@@ -11,6 +11,8 @@ data_dir = ROOT / "sample_data" / "images"
 csv_file = ROOT / "sample_data" / "midas_sample.csv"
 
 IMAGE_TYPE_MAP = {"dscope": 0,"6in": 1, "1ft": 2, "n/a - virtual": 3}
+GENDER_MAP = {"female": 0, "male": 1}
+
 
 class MIDASDataset(Dataset):
     def __init__(self, data_dir, csv_file, transform=None):
@@ -30,6 +32,10 @@ class MIDASDataset(Dataset):
         images = []
         image_types = []
         label = patient_rows["midas_melanoma"].iloc[0]
+        gender = str(patient_rows["midas_gender"].iloc[0])
+        gender = GENDER_MAP.get(gender, 0)
+        age = float(patient_rows["midas_age"].iloc[0])
+        metadata = torch.tensor([age, gender], dtype=torch.float32)
         for _, row in patient_rows.iterrows():
             image_name = row["midas_file_name"]
             image_path = os.path.join(self.data_dir, image_name)
@@ -41,4 +47,4 @@ class MIDASDataset(Dataset):
             image_types.append(image_type)
         images = torch.stack(images)
         image_types = torch.tensor(image_types)
-        return images, image_types, label
+        return images, image_types, metadata, label
