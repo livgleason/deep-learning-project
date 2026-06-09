28f16a55ac6864178af3f796ee70d1ca87b66591
Olivia Gleason <ogleason@n0301.talapas.uoregon.edu>
2026-06-09 09:48:50 -0700

    Final updates

--- src/midas/data/build_datasets.py
+++ src/midas/data/build_datasets.py
@@ -9,7 +9,7 @@ from midas.data.augmentation import augment_transform, standard_transform
 
 DATA_ROOT = os.environ.get("MIDAS_DATA_ROOT")
 if DATA_ROOT is None:
-    raise ValueError("Please set MIDAS_DATA_ROOT environment variable"
+    raise ValueError("Please set MIDAS_DATA_ROOT environment variable")
 
 MIDAS_IMG_DIR = os.path.join(DATA_ROOT, "MIDAS", "MIDAS_images")
 PAD_IMG_DIR = os.path.join(DATA_ROOT, "PAD-UFES-20", "PAD_images")
